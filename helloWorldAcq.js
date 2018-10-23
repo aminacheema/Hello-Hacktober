ad9c1a4b2405fc92b1cf772506e5bc18e271c204
Alvin Christian Quijano <alvin.quijano@live.ca>
2018-10-23 14:32:37 -0400

    Created HelloWorldAcq.js
    first hello world pull request.

---
+++ helloWorldAcq.js
@@ -0,0 +1,13 @@
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+
+    speak(){
+        console.log("Hello World!");
+    }
+}
+
+let acqno = new Person("Alvin Quijano");
+
+acqno.speak();
