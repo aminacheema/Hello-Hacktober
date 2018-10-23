e45a9fac6c6b461206e753fdb527d787b1ec00d2
Richard Bailey <richard@kohde.io>
2018-10-23 13:50:27 +0200

    updated typescript example

--- Languages/HelloWorld.ts
+++ Languages/HelloWorld.ts
@@ -1,1 +1,17 @@
-console.log("Hello, World!" as string);
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+function greeter(person: Person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+function hello() {
+    return "Hello, World!";
+}
+
+let user = { firstName: "Jane", lastName: "User" };
+
+document.body.innerHTML = hello();
+document.body.innerHTML = greeter(user);
