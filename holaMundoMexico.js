c1ac96b1b07fcaa791a1a679fb74afb3c3141474
Edgarzerocool <edgar_gonzalezja@hotmail.com>
2018-10-25 00:04:33 -0500

    my hello word from Mexico javascript

---
+++ holaMundoMexico.js
@@ -0,0 +1,6 @@
+var saludo="HOLA MUNDO DESDE MEXÍCO";
+var Reloj=new Date();
+var horas=Reloj.getHours();
+var minutos=Reloj.getMinutes();
+var segundos=Reloj.getSeconds();
+console.log(saludo+", la hora es "+horas+":"+minutos+":"+segundos);
