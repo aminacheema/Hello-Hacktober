d0cdc142cd3b4a8bfe823340d61e45e072b956d6
shahiarpit <33358464+shahiarpit@users.noreply.github.com>
2018-10-25 13:25:28 +0530

    Update Hello World.py
    Better use of print()

--- Languages/HelloWorld.py
+++ Languages/HelloWorld.py
@@ -1,1 +1,7 @@
+#prints Hello World
 print("Hello World")
+#Use of separation (sep) in print
+print("Hello", "world",sep='-')
+#Use of 'sep' and 'end' in print
+print("Hello",end='')
+print(" World")
