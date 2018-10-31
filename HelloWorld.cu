744a169b9fd7b61282072cb65b529dbdd2c09a7e
Savidude <mysticsavy@gmail.com>
2018-10-31 10:32:33 +0530

    Adding more messages to response set

--- HelloWorld.cu
+++ HelloWorld.cu
@@ -1,6 +1,9 @@
 var response = new Response;
 response.responseSet = [
-        "Hello world"
+        "Hello world",
+	"Hello. How may I help you?",
+	"Hello. How may I be of assistance?",
+	"Hello"
 ];
 response.userAction = "info";
 response.send;
